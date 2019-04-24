d2728bf586a52656e9ac245b0403ebd9daa00b8a
kinndongyoung <kicl6657@naver.com>
2019-04-24 19:15:17 +0900

    Create JumpScript.cs

---
+++ Testin/Assets/Script/JumpScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpScript : MonoBehaviour
+{
+    public float fJumpPower = 0.0f;
+    Rigidbody Player_rigidbody;
+    Animator Player_animator;
+    bool isJumping;
+
+    void Start()
+    {
+        Player_rigidbody = GetComponent<Rigidbody>();
+        Player_animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+
+            Player_rigidbody.AddForce(Vector3.up * fJumpPower, ForceMode.Impulse);
+            Player_animator.SetBool("isJumping", true);
+        }
+        else if (Input.GetButtonUp("Jump"))
+        {
+            isJumping = false;
+            Player_animator.SetBool("isJumping", false);
+        }
+    }
+}
