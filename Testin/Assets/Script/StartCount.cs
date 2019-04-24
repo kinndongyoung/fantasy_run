440c59e25e0d69040bf1547fe575e7cdaaba5319
kinndongyoung <kicl6657@naver.com>
2019-04-24 19:15:26 +0900

    Create StartCount.cs

---
+++ Testin/Assets/Script/StartCount.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartCount : MonoBehaviour
+{    
+    [HideInInspector]
+    public bool isRun;
+    private float fTime;
+
+    void start()
+    {
+        isRun = false;
+    }
+
+    void Update()
+    {       
+        if(fTime >= 3.0f)
+            isRun = true;
+        else if(fTime < 3.0f)
+            fTime += Time.deltaTime;
+    }
+}
