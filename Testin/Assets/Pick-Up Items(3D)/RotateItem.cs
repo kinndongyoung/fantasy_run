89a6268a373c57e029a4f260a78e1d7d0b62f5a9
kinndongyoung <kicl6657@naver.com>
2019-04-24 19:14:51 +0900

    Create RotateItem.cs

---
+++ Testin/Assets/Pick-Up Items(3D)/RotateItem.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateItem : MonoBehaviour {
+
+	public float Xspeed,Yspeed = -100f,Zspeed;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.Rotate(Time.deltaTime*Xspeed, Time.deltaTime*Yspeed, Time.deltaTime*Zspeed, Space.World);
+	}
+}
