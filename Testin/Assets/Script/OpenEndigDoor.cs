5641a5185d16e1bff053a68c240a1d01a1eaac70
kinndongyoung <kicl6657@naver.com>
2019-04-24 19:15:23 +0900

    Create OpenEndigDoor.cs

---
+++ Testin/Assets/Script/OpenEndigDoor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenEndigDoor : MonoBehaviour
+{
+    public float MovePosY = 0.0f;
+    private int count = 0;
+
+    bool isOpen = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && !isOpen) Open();
+    }
+
+    void Open()
+    {
+        isOpen = true;
+        count++;
+
+        Vector3 Pos = transform.localPosition;
+        Pos.y += MovePosY;
+
+        transform.localPosition = Pos;
+
+        if (count < 90)
+            Invoke("Open", Time.deltaTime);
+        else
+        {
+            count = 0;
+            isOpen = false;
+        }            
+    }
+}
