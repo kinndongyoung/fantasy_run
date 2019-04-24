75f73d27981bc70183791ece40a9f9e71d2beba7
kinndongyoung <kicl6657@naver.com>
2019-04-24 19:15:12 +0900

    Create EndTrigger.cs

---
+++ Testin/Assets/Script/EndTrigger.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndTrigger : MonoBehaviour
+{
+    public GameObject EndWindow;
+    public GameObject Player;
+    public TimeCount LapTime;
+    
+    private Animator Player_animator;
+    
+    private float fTime;
+
+    void Start()
+    {
+        EndWindow = GameObject.Find("EndWindow");
+        LapTime = GameObject.Find("LapTime").GetComponent<TimeCount>();
+        Player_animator = Player.GetComponent<Animator>();
+
+        EndWindow.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Player_animator.SetBool("EndMotion", true);
+            LapTime.Time_bState = false;
+        }            
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            fTime += Time.deltaTime;
+
+            if(fTime >= 1.5f)
+            {
+                EndWindow.SetActive(true);
+            }
+        }
+    }
+}
