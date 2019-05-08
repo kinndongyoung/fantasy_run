f9435d30e0fedcfb222d0961e630ccc387052a30
seongji0125 <49138648+seongji0125@users.noreply.github.com>
2019-05-08 18:06:19 +0900

    Create Test.cs

---
+++ Testin/Assets/Script/Test.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour
+{
+    public Box_Rotate box_rotate;
+
+    // Start is called befsore the first frame update
+    void Start()
+    {
+        box_rotate.GetComponent<Box_Rotate>();
+       // if(box_rotate.state == false)
+       // this.gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            if (box_rotate.state == false)
+            this.gameObject.SetActive(false);
+            
+
+
+    }
+}
