418f4e87816bce1a8f6314a58d80bc7593e2168c
kinndongyoung <kicl6657@naver.com>
2019-05-16 17:27:10 +0900

    Create TestMove.cs

---
+++ Testin/Assets/Script/TestScript/TestMove.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMove : MonoBehaviour
+{
+    public float fForwardSpd = 5.0f;
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * fForwardSpd * Time.deltaTime);
+    }
+}
