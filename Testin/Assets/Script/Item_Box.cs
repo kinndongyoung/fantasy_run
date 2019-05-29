12f24bb08ae06ab6344fef7ead466a555ce03c3f
seongji0125 <49138648+seongji0125@users.noreply.github.com>
2019-05-30 00:07:29 +0900

    Update Item_Box.cs

--- Testin/Assets/Script/Item_Box.cs
+++ Testin/Assets/Script/Item_Box.cs
@@ -8,14 +8,14 @@ public class Item_Box : MonoBehaviour
     //public float Xspeed,Yspeed = -100f,Zspeed;
     public float Yspeed = 70;
     public int iRandom;// = Random.Range(0 , 2);
-    public GameObject Speed_Item1, Speed_Item2, Igonore_Item;
+    public GameObject Speed_Item1, Speed_Item2, Ignore_Item;
 
-    public bool Speed1_state, Speed2_state, Igonore_state;
+    public bool Speed1_state, Speed2_state, Ignore_state;
     // Update is called once per frame
     private void Start()
     {
         Speed_Item1 = GameObject.FindGameObjectWithTag("Item");
-        Igonore_Item = GameObject.FindGameObjectWithTag("Item1");
+        Ignore_Item = GameObject.FindGameObjectWithTag("Item1");
         Speed_Item2 = GameObject.FindGameObjectWithTag("Item2");
        
     }
@@ -24,7 +24,7 @@ public class Item_Box : MonoBehaviour
     {
         Speed1_state = false;
         Speed2_state = false;
-        Igonore_state = false;
+        Ignore_state = false;
         this.transform.Rotate(0, Time.deltaTime * Yspeed, 0, Space.World);
         //this.transform.Rotate(new Vector3(0, 0, 70) * Time.deltaTime);
         //transform.Rotate(Time.deltaTime*Xspeed, Time.deltaTime*Yspeed, Time.deltaTime*Zspeed, Space.World);
@@ -33,7 +33,7 @@ public class Item_Box : MonoBehaviour
     }
     void OnTriggerEnter(Collider other)
     {
-        iRandom = Random.Range(0, 1);
+        iRandom = Random.Range(0, 5);
         //플레이어가 아이템에 닿았을 때   
         if (other.gameObject.tag == "Player")
         {
@@ -51,18 +51,18 @@ public class Item_Box : MonoBehaviour
       
         if (iRandom == 0 || iRandom == 3)
         {
-            Speed_Item1.gameObject.SetActive(true);
+            Speed_Item2.gameObject.SetActive(true);
             Speed1_state = true;
         }
         else if (iRandom == 1 || iRandom == 4)
         {
-            Speed_Item2.gameObject.SetActive(true);
+            Speed_Item1.gameObject.SetActive(true);
             Speed2_state = true;
         }
         else if (iRandom == 2 || iRandom == 5)
         {
-            Igonore_Item.gameObject.SetActive(true);
-            Igonore_state = true;
+            Ignore_Item.gameObject.SetActive(true);
+            Ignore_state = true;
         }
         yield return new WaitForSeconds(0.5f);
     }
