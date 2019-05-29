7cfc676198d8925ec9f3c93f2d7cc5fdd3287010
seongji0125 <49138648+seongji0125@users.noreply.github.com>
2019-05-29 19:28:28 +0900

    Update ItemEffet.cs

--- Testin/Assets/Script/ItemEffet.cs
+++ Testin/Assets/Script/ItemEffet.cs
@@ -10,11 +10,11 @@ public class ItemEffet : MonoBehaviour
     public Item_Box Item_Box;
     private GameObject taget;
     
-    public bool bState;
+    public bool Speed1_state, Speed2_state, Igonore_state;
 
     void Start()
     {
-        bState = false;
+
         spline_Move = GameObject.FindGameObjectWithTag("Player").GetComponent<SWS.splineMove>();
         Item_Box.GetComponent<Item_Box>();
     }
@@ -26,14 +26,41 @@ public class ItemEffet : MonoBehaviour
 
     void Update()
     {
-        if (Item_Box.state == false)
+        if (Item_Box.Speed1_state == false)
+            this.gameObject.SetActive(false);
+        if (Item_Box.Speed2_state == false)
+            this.gameObject.SetActive(false);
+        if (Item_Box.Igonore_state == false)
             this.gameObject.SetActive(false);
 
-        if(Input.GetMouseButton(0))
+        if (Item_Box.Speed1_state == true)
+        {
+            this.gameObject.SetActive(true);
+        }
+        if (Item_Box.Speed2_state == true)
+        {
+            this.gameObject.SetActive(true);
+        }
+        if (Item_Box.Igonore_state == true)
+        {
+            this.gameObject.SetActive(true);
+        }
+        if (Input.GetMouseButton(0))
         {
             if(EventSystem.current.IsPointerOverGameObject())
             {
-                bState = true;
+                if (Item_Box.Speed1_state == true)
+                {
+                    Speed1_state = true;
+                }
+                if (Item_Box.Speed2_state == true)
+                {
+                    Speed2_state = true;
+                }
+                if (Item_Box.Igonore_state == true)
+                {
+                    Igonore_state = true;
+                }
                 this.gameObject.SetActive(false);
             }
         }
