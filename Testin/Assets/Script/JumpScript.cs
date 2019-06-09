a42ad8408e64b5da2d790453e7fcfe0f8f19f783
LeeSangHwee <tkdgnl56@naver.com>
2019-06-10 04:14:59 +0900

    Update JumpScript.cs

--- Testin/Assets/Script/JumpScript.cs
+++ Testin/Assets/Script/JumpScript.cs
@@ -14,9 +14,8 @@ public class JumpScript : MonoBehaviour
     private float fTime = 0.0f;
     private Vector3 Temp_PlayerPos;
     public float fJumpPower = 0.0f;
-    public GameObject Player;
-    Rigidbody Player_rigidbody;
-    CapsuleCollider Player_Collider;
+    public Rigidbody Player_rigidbody;
+    public GameObject Player_Col;
     Animator Player_animator;
     bool isJumping;
     int bCheck;
@@ -24,65 +23,36 @@ public class JumpScript : MonoBehaviour
     void Start()
     {
         bCheck = (int)STATE.eIDLE;
-
-        Player_Collider = GetComponent<CapsuleCollider>();
-        Player_rigidbody = GetComponent<Rigidbody>();
         Player_animator = GetComponent<Animator>();
     }
-    //505 -247 0
-    //0.8 0.8 1
+
     void Update()
     {
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && bCheck == (int)STATE.eIDLE)
         {
             isJumping = true;
             bCheck = (int)STATE.eUP;
+
+            Player_Col.SetActive(false);
             Player_rigidbody.AddForce(Vector3.up * fJumpPower, ForceMode.Impulse);
             Player_animator.SetBool("isJumping", true);
-
-            Temp_PlayerPos = Player.transform.localPosition;
         }
-        else if (Input.GetButtonUp("Jump"))
+        else if (Input.GetButtonUp("Jump") && bCheck == (int)STATE.eUP)
         {
-            isJumping = false;
+            isJumping = false;            
             Player_animator.SetBool("isJumping", false);
         }
 
-        //콜리더 박스가 올라갔다 내려갔다.
-        //Player_Collider.center = new Vector3(0, 1.3f, 0);
-        //
-        //switch (bCheck)
-        //{
-        //    case (int)STATE.eIDLE:
-        //    {
-        //        Player_Collider.center = new Vector3(0, 1.3f, 0);
-        //    }
-        //    break;
-        //    case (int)STATE.eUP:
-        //    {
-        //        Player_Collider.center = new Vector3(0, (fJumpPower * Time.deltaTime), 0);
-        //        fTime += Time.deltaTime;
-        //
-        //        if (Player.transform.localPosition.y >= Temp_PlayerPos.y + 10)
-        //        {
-        //            bCheck = (int)STATE.eDOWN;
-        //            fTime = 0.0f;
-        //        }
-        //    }
-        //    break;
-        //    case (int)STATE.eDOWN:
-        //    {
-        //        Player_Collider.center = new Vector3(0, ((-fJumpPower) * Time.deltaTime), 0);
-        //        fTime += Time.deltaTime;
-        //
-        //        if (Player.transform.localPosition.y <= Temp_PlayerPos.y)
-        //        {
-        //            bCheck = (int)STATE.eIDLE;
-        //            Player.transform.localPosition.Set(Temp_PlayerPos.x, Temp_PlayerPos.y, Temp_PlayerPos.z);
-        //            fTime = 0.0f;
-        //        }                        
-        //    }
-        //    break;
-        //}
+        if(bCheck == (int)STATE.eUP)
+        {
+            fTime += Time.deltaTime;
+
+            if (fTime >= 1.13f)
+            {
+                Player_Col.SetActive(true);
+                bCheck = (int)STATE.eIDLE;
+                fTime = 0.0f;
+            }
+        }
     }
 }
