d8517e71b4f433c4b8760afd511cf08e53f8273a
LeeSangHwee <tkdgnl56@naver.com>
2019-06-10 04:15:03 +0900

    Create PlayerColMove.cs

---
+++ Testin/Assets/Script/PlayerColMove.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerColMove : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject Player_Col;
+
+    void Update()
+    {
+        Player_Col.transform.localPosition = Player.transform.localPosition;
+    }
+}
